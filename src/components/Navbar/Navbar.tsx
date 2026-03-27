863a6a2529399ee4ae41ccc5e63d9618dfb89045
Navi <naruebet.singsathon@hotmail.com>
2026-03-27 10:52:17 +0100

    new style

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -6,10 +6,11 @@ export default function Navbar() {
     <header className="nav">
       <div className="nav__inner">
         <div className="nav__brand">
+          <NavLink to="/">
           <span className="nav__logo">🤖</span>
           <span>Star Wars Encyclopedia</span>
+          </NavLink>
         </div>
-
         <nav className="nav__links">
           <NavLink to="/films">Films</NavLink>
           <NavLink to="/people">People</NavLink>
